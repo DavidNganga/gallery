5a0ff0f1e5d6572c186f866acc989d50b63af4cb
DavidNganga <ngashiedavid@gmail.com>
2018-05-03 21:48:23 +0300

    installed pillow

--- gallery/pics/models.py
+++ gallery/pics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_editor(self):
+        self.save()
+
+    class Meta:
+        ordering = ['name']
+
+class location(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+class Image(models.Model):
+     name = models.CharField(max_length=20)
+     description = models.CharField(max_length=200)
+     location = models.ManyToManyField(location)
+     category = models.ForeignKey(Editor)
--- gallery/pics/tests.py
+++ gallery/pics/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Editor,Image,location
+
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.david= Editor(name = 'David')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.david,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.david.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
