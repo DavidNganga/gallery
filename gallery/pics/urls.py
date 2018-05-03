541144bc4115e34a20a091480db9ad5802565d5c
DavidNganga <ngashiedavid@gmail.com>
2018-05-03 16:17:07 +0300

    created a database

--- gallery/pics/urls.py
+++ gallery/pics/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.visualize,name = 'visualize'),
-    url('^today/$',views.pic_day,name='picsToday')
+    url(r'^$',views.visualize,name = 'visualize'),
+
 ]
