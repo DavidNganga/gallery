824752f138da029cfb8e29abb6f92fe88fd0ad55
DavidNganga <ngashiedavid@gmail.com>
2018-05-07 12:39:53 +0300

    changed models

---
+++ gallery.3/pics/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+class Editor(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_editor(self):
+        self.save()
+
+    class Meta:
+        ordering = ['name']
+
+class Location(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Image(models.Model):
+     name = models.CharField(max_length=20)
+     description = models.CharField(max_length=200)
+     location = models.ForeignKey(Location, null=True)
+     category = models.ForeignKey(Category,null=True)
+     Image_image = models.ImageField(upload_to = 'images/',null = True)
+
+     @classmethod
+     def get_all(cls):
+         pics = cls.objects.all()
+         return pics
+
+     # @classmethod
+     # def search_by_category(cls,searched_category):
+     #     images = cls.objects.filter(category__name__icontains= searched_category)
+     #     return images
+
+     def get_Image_by_id(cls,id):
+        images = cls.objects.get(id=id)
+        return images
+
+     @classmethod
+     def search_results(cls,search_term):
+         pics = cls.objects.filter(category__name__icontains=search_term)
+         print(pics)
+         return pics
---
+++ gallery.3/pics/views.py
@@ -0,0 +1,32 @@
+from django.http  import HttpResponse
+from django.shortcuts import render
+from .models import Image,Category,Location,Editor
+
+# Create your views here.
+def visualize(request):
+    photos = Image.get_all()
+    return render (request, 'welcome.html',{"photos":photos})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        print(search_term)
+
+        images = Image.search_results(search_term)
+        message = f"{search_term}"
+        print(images)
+
+        return render(request, 'search.html',{"message":message,"images": images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+
+def all(request,image_id):
+
+    image = Image.objects.get(id = image_id)
+
+    return render(request,"all-stuff.html",{"image":image, id: image_id})
